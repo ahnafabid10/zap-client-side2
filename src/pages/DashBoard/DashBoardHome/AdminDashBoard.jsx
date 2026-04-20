4a12b20aade50a91b67ef0c37097c71f0f36bf40
Ahnaf Abid <ahnafabid600@gmail.com>
2026-04-20 23:52:32 +0600

    added MongoDB Aggregation & Pipeline annd show that in ui

--- src/pages/DashBoard/DashBoardHome/AdminDashBoard.jsx
+++ src/pages/DashBoard/DashBoardHome/AdminDashBoard.jsx
@@ -1,9 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const AdminDashBoard = () => {
+    const axiosSecure= useAxiosSecure()
+    const {data: deliveryStats = []} = useQuery({
+        queryKey: ['delivery-Status-stats'],
+        queryFn: async()=>{
+            const res = await axiosSecure.get(`/parcels/delivery-status/stats`)
+            return res.data
+        }
+    })
     return (
         <div>
-            Admin Dashboard
+            <h2 className='text-4xl'>Admin Dashboard</h2>
+            <div className="stats shadow">
+                {
+                    deliveryStats.map(stat=>
+                          <div key={stat._id} className="stat">
+    <div className="stat-figure text-secondary">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="inline-block h-8 w-8 stroke-current"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+    </div>
+    <div className="stat-title">{stat._id}</div>
+    <div className="stat-value">{stat.count}</div>
+    <div className="stat-desc">Jan 1st - Feb 1st</div>
+  </div>
+                    )
+                }
+
+</div>
         </div>
     );
 };
